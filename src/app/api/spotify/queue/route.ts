1408305a13a9673bcc0802ea7ebfe5ddf6086e84
neoncodes <41237443+charleywolf@users.noreply.github.com>
2024-06-27 10:24:43 -0400

    events door

--- src/app/api/spotify/queue/route.ts
+++ src/app/api/spotify/queue/route.ts
@@ -1,19 +1,28 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+
 import { auth } from "@/auth";
 import getQueue from "@/lib/fetch/getQueue";
 import isAdminSession from "@/lib/isAdminSession";
 
-export async function GET() {
-  const session = await auth();
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("code");
 
-  if (!session) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+  if (query && query === process.env.NEXT_EVENTS_KEY) {
+    const queue = await getQueue();
+    return NextResponse.json(queue);
+  } else {
+    const session = await auth();
+
+    if (!session) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    if (!isAdminSession(session)) {
+      return NextResponse.json({ message: "Forbidden" }, { status: 403 });
+    }
+
+    const queue = await getQueue();
+    return NextResponse.json(queue);
   }
-
-  if (!isAdminSession(session)) {
-    return NextResponse.json({ message: "Forbidden" }, { status: 403 });
-  }
-
-  const queue = await getQueue();
-  return NextResponse.json(queue);
 }
