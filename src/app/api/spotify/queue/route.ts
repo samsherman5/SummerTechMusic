ca7ef5ead8be115dd023510b53703d859da00643
neoncodes <41237443+charleywolf@users.noreply.github.com>
2024-06-28 09:24:31 -0400

    choose port

--- src/app/api/spotify/queue/route.ts
+++ src/app/api/spotify/queue/route.ts
@@ -7,21 +7,10 @@ import isAdminSession from "@/lib/isAdminSession";
 export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams;
   const query = searchParams.get("code");
+  console.log(query);
+  console.log(process.env.NEXT_EVENTS_KEY);
 
   if (query && query === process.env.NEXT_EVENTS_KEY) {
-    const queue = await getQueue();
-    return NextResponse.json(queue);
-  } else {
-    const session = await auth();
-
-    if (!session) {
-      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-    }
-
-    if (!isAdminSession(session)) {
-      return NextResponse.json({ message: "Forbidden" }, { status: 403 });
-    }
-
     const queue = await getQueue();
     return NextResponse.json(queue);
   }
